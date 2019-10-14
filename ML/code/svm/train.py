8739bb75406946acc1c6ebf9adf13c088e454085
윤형준 <dbslzhem16@gmail.com>
2019-10-14 22:57:38 +0900

    model mode implemented

--- ML/code/svm/train.py
+++ ML/code/svm/train.py
@@ -6,19 +6,25 @@ from sklearn.ensemble import RandomForestClassifier
 
 def load_data(file_dir):
 	df = pd.read_csv(file_dir)
-	df = df.loc[:len(df)//5*4]
-	y = df['label'].tolist()
-	X = df.drop('label', axis=1).values.tolist()
+	df.sample(frac=1)
+	df_train = df.loc[:len(df)//5*4]
+	y = df_train['label'].tolist()
+	X = df_train.drop('label', axis=1).values.tolist()
 
-	return X, y
+	df_test = df.loc[len(df)//5*4:]
+	y_test = df_test['label'].tolist()
+	X_test = df_test.drop('label', axis=1).values.tolist()
+
+	return X, y, X_test, y_test
 
 
-def train(mode, X, y):
+def train(mode, X, y, X_test, y_test):
 	if mode == "svm":
 		model = svm.SVC(kernel='poly', C=1.0)
 	if mode == "randomforest":
 		model = RandomForestClassifier()
 	model.fit(X, y)
+	model.score(X_test, y_test)
 
 	return model
 
@@ -29,6 +35,6 @@ def save_model(model, filename):
 
 
 if __name__ == "__main__":
-	X, y = load_data(sys.argv[3])
-	model = train(sys.argv[1], X, y)
+	X, y, X_test, y_test = load_data(sys.argv[3])
+	model = train(sys.argv[1], X, y, X_test, y_test)
 	save_model(model, sys.argv[2])
